bf30ec754931d1bd99825870097e366b7a4e4844
Arijit <arijitdasofficial@gmail.com>
2022-05-23 20:13:39 +0530

    new-files

--- 04_operators/04-06_bitwise-operators.js
+++ 04_operators/04-06_bitwise-operators.js
@@ -1,20 +1,149 @@
-// JavaScript Bitwise Operators
-// Bit operators work on 32 bits numbers.
+/*
+• JavaScript Bitwise Operators
+• Bit operators work on 32 bits numbers
+• Any numeric operand in the operation is converted into a 32 bit number
+• The result is converted back to a JavaScript number
 
-// Any numeric operand in the operation is converted into a 32 bit number. The result is converted back to a JavaScript number.
-// Operator	    Description	            Example	        Same as	        Result	    Decimal
-// &	        AND	                    5 & 1	        0101 & 0001	    0001	    1
-// |	        OR	                    5 | 1	        0101 | 0001	    0101	    5
-// ~	        NOT	                    ~ 5	            ~0101	        1010	    10
-// ^	        XOR	                    5 ^ 1	        0101 ^ 0001	    0100	    4
-// <<	        left shift	            5 << 1	        0101 << 1	    1010	    10
-// >>	        right shift	            5 >> 1	        0101 >> 1	    0010	    2
-// >>>	        unsigned right shift	5 >>> 1	        0101 >>> 1	    0010	    2
+    Operator	Description	            Example	        Same as	        Result	    Decimal
+    &	        AND	                    5 & 1	        0101 & 0001	    0001	    1
+    |	        OR	                    5 | 1	        0101 | 0001	    0101	    5
+    ~	        NOT	                    ~ 5	            ~0101	        1010	    -6
+    ^	        XOR	                    5 ^ 1	        0101 ^ 0001	    0100	    4
+    <<	        left shift	            5 << 1	        0101 << 1	    1010	    10
+    >>	        right shift	            5 >> 1	        0101 >> 1	    0010	    2
+    >>>	        unsigned right shift	5 >>> 1	        0101 >>> 1	    0010	    2
 
+
+• The operands are converted to 32-bit integers and expressed by a series of bits (zeroes and ones)
+• Numbers with more than 32 bits get their most significant bits discarded
+
+    Before: 11100110111110100000000000000110000000000001
+    After:              10100000000000000110000000000001
+
+*/
+
+
+let x = 1                   // ...0001 -- (63 0's)(1)       -- 64bit
+let y = 2                   // ...0010 -- (62 0's)(1)(0)    -- 64bit
+let z = 10
+console.log(z.toString(2));
+console.log(parseInt('1010', 2));
+
+console.log("---------------------------------------------------------------------------------------------------------------------------------");
+
+/*
+• Bitwise AND (&)
+• The bitwise AND operator (&) returns a 1 in each bit position for which the corresponding bits of both operands are 1s
+*/
+
+
+let a = 5;                  // 00000000000000000000000000000101
+let b = 3;                  // 00000000000000000000000000000011
+
+console.log(a & b);         // 00000000000000000000000000000001     // expected output: 1
+
+
+console.log("---------------------------------------------------------------------------------------------------------------------------------");
 
 
 /*
-• Bitwise AND
-• When a bitwise AND is performed on a pair of bits, it returns 1 if both bits are 1
+• Bitwise OR (|)
+• The bitwise OR operator (|) returns a 1 in each bit position for which the corresponding bits of either or both operands are 1s
 */
+
+a = 5;                      // 00000000000000000000000000000101
+b = 3;                      // 00000000000000000000000000000011
+
+console.log(a | b);         // 00000000000000000000000000000111     // expected output: 7
+
+
+console.log("---------------------------------------------------------------------------------------------------------------------------------");
+
+
+/*
+• Bitwise XOR (^)
+• The bitwise XOR operator (^) returns a 1 in each bit position for which the corresponding bits of either but not both operands are 1s
+*/
+
+a = 5;                      // 00000000000000000000000000000101
+b = 3;                      // 00000000000000000000000000000011
+
+console.log(a ^ b);         // 00000000000000000000000000000110     // expected output: 6
+
+
+console.log("---------------------------------------------------------------------------------------------------------------------------------");
+
+
+/*
+• Bitwise NOT (~)
+• The bitwise NOT operator (~) inverts the bits of its operand. 
+• Like other bitwise operators, it converts the operand to a 32-bit signed integer
+*/
+
+a = 5;                      // 00000000000000000000000000000101
+b = -3;                     // 11111111111111111111111111111101
+
+console.log(~a);            // 11111111111111111111111111111010        // expected output: -6
+
+console.log(~b);            // 00000000000000000000000000000010        // expected output: 2
+
+
+console.log("---------------------------------------------------------------------------------------------------------------------------------");
+
+
+/*
+• Left shift (<<)
+• The left shift operator (<<) shifts the first operand the specified number of bits to the left
+• Excess bits shifted off to the left are discarded
+• Zero bits are shifted in from the right
+*/
+
+a = 5;                      // 00000000000000000000000000000101
+b = 2;                      // 00000000000000000000000000000010
+
+console.log(a << b);        // 00000000000000000000000000010100        // expected output: 20
+
+
+console.log("---------------------------------------------------------------------------------------------------------------------------------");
+
+
+/*
+• Right shift (>>)
+• The right shift operator (>>) shifts the first operand the specified number of bits to the right
+• Excess bits shifted off to the right are discarded
+• Copies of the leftmost bit are shifted in from the left
+• Since the new leftmost bit has the same value as the previous leftmost bit, the sign bit (the leftmost bit) does not change
+• Hence the name "sign-propagating"
+*/
+
+a = 5;                      //  00000000000000000000000000000101
+b = 2;                      //  00000000000000000000000000000010
+let c = -5;                 //  11111111111111111111111111111011
+
+console.log(a >> b);        //  00000000000000000000000000000001        // expected output: 1
+
+
+console.log(c >> b);        //  11111111111111111111111111111110       // expected output: -2
+
+
+
+console.log("---------------------------------------------------------------------------------------------------------------------------------");
+
+
+/*
+• Unsigned right shift (>>>)
+• The unsigned right shift operator (>>>) (zero-fill right shift) shifts the first operand the specified number of bits to the right
+• 
+*/
+
+a = 5;                      //  00000000000000000000000000000101
+b = 2;                      //  00000000000000000000000000000010
+c = -5;                     //  11111111111111111111111111111011
+
+console.log(a >>> b);       //  00000000000000000000000000000001      // expected output: 1
+
+console.log(c >>> b);       //  00111111111111111111111111111110      // expected output: 1073741822
+
+
+console.log("---------------------------------------------------------------------------------------------------------------------------------");
 
--- 21_Events/21-01_events.js
+++ 21_Events/21-01_events.js
@@ -6,10 +6,32 @@ const li_1 = document.getElementById('li-1');
 button_1.addEventListener('click', addli);
 button_2.addEventListener('click', deleteli);
 
-function addli(e){
-    li_1.innerText = "someone clicked button 1 !";
-    console.log(e);
+function addli(e, value = 'value'){                              // e is event object, it contains all the event properties
+    li_1.innerText = "someone clicked button 1 ! " + value;
+    console.log(e, e.target);
 }
 function deleteli(){
     li_1.innerText = "";
 }
+
+
+console.log("---------------------------------------------------------------------------------------------------------------------------------");
+
+
+const form_btn = document.getElementById('form-btn');
+form_btn.addEventListener('click', submit);
+
+function submit(e){
+    console.log(e);
+}
+
+const h1 = document.getElementsByTagName('h1')[0];
+h1.addEventListener('dblclick', h1func);
+function h1func(e){
+    console.log(e);
+}
+
+document.getElementById('body').addEventListener('mousemove', function(e){
+    document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, ${120})`
+    console.log(e.offsetX, e.offsetY);
+})
