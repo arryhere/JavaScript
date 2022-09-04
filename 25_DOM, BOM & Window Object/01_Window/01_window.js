d33be8e91bca55dcfde1ba9abe482eae87f012f3
Arijit <arijitdasofficial@gmail.com>
2022-09-04 19:46:02 +0530

    new files

--- 25_DOM, BOM & Window Object/01_Window/01_window.js
+++ 25_DOM, BOM & Window Object/01_Window/01_window.js
@@ -1,2 +1,11 @@
+/*
+•       window
+      /   |   \
+    DOM  BOM  JavaScript Core
+
+• Window object represents browser window and provides methods to control it. 
+• It is a global object
+*/
+
 
 console.table(window)
---
+++ 25_DOM, BOM & Window Object/02_BOM/01_BOM.js
@@ -0,0 +1,7 @@
+/*
+• BOM - Browser Object Model
+• The Browser Object Model represents additonal objects provided by the browser (Host Environment)
+    for working with everything except the document.
+• The fucntions - alert / confirm / prompt are also a part of the BOM.
+• 
+*/
